f71f013de4953717d164c6246ba43d346542e629
seb43654 <sebsmith3@gmail.com>
2021-10-01 08:32:40 +0100

    revert

--- selfdrive/car/ocelot/radar_interface.py
+++ selfdrive/car/ocelot/radar_interface.py
@@ -35,9 +35,9 @@ class RadarInterface(RadarInterfaceBase):
   def update(self, can_strings):
     if self.no_radar:
       #time.sleep(self.radar_ts)
-      #time.sleep(0.02)
-      #return car.RadarData.new_message()
-      return super().update(None)
+      #return super().update(None)
+      time.sleep(0.02)
+      return car.RadarData.new_message()
     
     vls = self.rcp.update_strings(can_strings)
     self.updated_messages.update(vls)
