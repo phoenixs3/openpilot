6739a7f5a0ecd197a3c7e468b9decbbf1fa3582f
Seb <sebsmith3@gmail.com>
2023-03-30 19:55:44 +0100

    update paint.cc to have 2 decimal points on boost pressure

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -605,7 +605,7 @@ static void bb_ui_draw_measures_right(UIState *s, int bb_x, int bb_y, int bb_w )
     if(s->scene.boostPressure < 0) {
       snprintf(val_str, sizeof(val_str), "0");
     }
-    else {snprintf(val_str, sizeof(val_str), "%.0f", (s->scene.boostPressure));}
+    else {snprintf(val_str, sizeof(val_str), "%.2f", (s->scene.boostPressure / 10));}
     snprintf(uom_str, sizeof(uom_str), "bar");
     bb_h +=bb_ui_draw_measure(s,  val_str, uom_str, "BOOST",
         bb_rx, bb_ry, bb_uom_dx,
