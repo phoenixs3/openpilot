8685d3fef2041c05550060d09effd3d5e1847189
Seb <sebsmith3@gmail.com>
2023-04-14 17:34:35 +0100

    increase brakemax, stricter driver monitoring parameters

--- selfdrive/car/ocelot/interface.py
+++ selfdrive/car/ocelot/interface.py
@@ -79,8 +79,8 @@ class CarInterface(CarInterfaceBase):
     ret.gasMaxV = [0.24, 0.3, 0.35, 0.5]
 
     #Brake maximum values
-    ret.brakeMaxBP = [0., 35]             #0mph, 78mph
-    ret.brakeMaxV = [.28, .45]            #0.26*20 = 5.2mm, 0.45*20 = 9mm
+    ret.brakeMaxBP = [0., 2., 35]             #0mph, 78mph
+    ret.brakeMaxV = [.26, 0.32,.45]            #0.26*20 = 5.2mm, 0.45*20 = 9mm
 
     return ret
 
--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -14,10 +14,10 @@ EventName = car.CarEvent.EventName
 #  We recommend that you do not change these numbers from the defaults.
 # ******************************************************************************************
 
-_AWARENESS_TIME = 120.  # passive wheel touch total timeout
+_AWARENESS_TIME = 120.  # passive wheel touch total timeout (default 35)
 _AWARENESS_PRE_TIME_TILL_TERMINAL = 12.
 _AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.
-_DISTRACTED_TIME = 20.
+_DISTRACTED_TIME = 11.    #default 11
 _DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
 _DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
 
