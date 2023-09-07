dec12cea7c8d2ecf47307e8690767dd3897261a2
Seb <sebsmith3@gmail.com>
2023-09-07 15:40:19 +0100

    re-instate registration, revert server back to comma.ai

--- selfdrive/ui/qt/widgets/setup.cc
+++ selfdrive/ui/qt/widgets/setup.cc
@@ -76,7 +76,7 @@ PrimeUserWidget::PrimeUserWidget(QWidget* parent) : QWidget(parent) {
   mainLayout = new QVBoxLayout;
   mainLayout->setMargin(30);
 
-  QLabel* commaPrime = new QLabel("RETROPILOT");
+  QLabel* commaPrime = new QLabel("OPENPILOT");
   mainLayout->addWidget(commaPrime, 0, Qt::AlignTop);
 
   username = new QLabel();
@@ -109,7 +109,7 @@ PrimeUserWidget::PrimeUserWidget(QWidget* parent) : QWidget(parent) {
     return;
   }
 
-  QString url = "https://pilot.openpilot.co.za/v1/devices/" + dongleId + "/owner";
+  QString url = "https://api.commadotai.com/v1/devices/" + dongleId + "/owner";
   RequestRepeater *repeater = new RequestRepeater(this, url, "ApiCache_Owner", 6);
   QObject::connect(repeater, &RequestRepeater::receivedResponse, this, &PrimeUserWidget::replyFinished);
 }
@@ -241,7 +241,7 @@ SetupWidget::SetupWidget(QWidget* parent) : QFrame(parent) {
 
   // set up API requests
   QString dongleId = QString::fromStdString(Params().get("DongleId"));
-  QString url = "https://pilot.openpilot.co.za/v1.1/devices/" + dongleId + "/";
+  QString url = "https://api.commadotai.com/v1.1/devices/" + dongleId + "/";
   RequestRepeater* repeater = new RequestRepeater(this, url, "ApiCache_Device", 5);
 
   QObject::connect(repeater, &RequestRepeater::receivedResponse, this, &SetupWidget::replyFinished);
